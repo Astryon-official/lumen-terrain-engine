ffe04e82ffa4a980218857d3e5fa3c9f01ae7b45
TechGaming-TG <foodmans12345@gmail.com>
2026-07-13 18:14:37 +1000

    Implement LumenTerrainEngine as ModInitializer

--- src/main/java/com/astryon/lte/LumenTerrainEngine.java
+++ src/main/java/com/astryon/lte/LumenTerrainEngine.java
@@ -1,1 +1,14 @@
+package com.astryon.lte;
 
+import net.fabricmc.api.ModInitializer;
+
+public class LumenTerrainEngine implements ModInitializer {
+
+    public static final String MOD_ID = "lumenterrain";
+
+    @Override
+    public void onInitialize() {
+        System.out.println("[Lumen Terrain Engine] Initializing...");
+        System.out.println("[Lumen Terrain Engine] Core systems online.");
+    }
+}
